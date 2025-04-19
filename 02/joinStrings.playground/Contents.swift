763fe735410bbbd06661f1b6adea281707651163
Thiago Aires da Silva <makarrao@gmail.com>
2025-04-19 11:26:42 -0300

    Join Strings

---
+++ 02/joinStrings.playground/Contents.swift
@@ -0,0 +1,19 @@
+import Cocoa
+
+// joining with plus(+)
+let people = "Haters"
+let action = "hate"
+
+let lyric = people + " gonna " + action
+
+// String interpolation
+
+let name = "Taylor"
+let age = 26
+let message = "Hello, my name is \(name) and i'm \(age) years old"
+
+let number = 11
+let missionMesage = "Apollo " + String(number) + " landed on the moon."
+let missionMesageInterpolation = "Apollo \(number) landed on the moon."
+
+let calc = "5 x 5 equals \(5 * 5)"
