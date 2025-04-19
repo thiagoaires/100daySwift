7bc8c88481c4fa6e0d229dce9a955faa5f3d4379
Thiago Aires da Silva <makarrao@gmail.com>
2025-04-19 11:35:01 -0300

    Checkpoint 01

---
+++ 02/checkpoint01.playground/Contents.swift
@@ -0,0 +1,12 @@
+import Cocoa
+
+
+
+// farenheight formula
+// (value * 9)/5 + 32
+
+let celsiusValue = 37.2
+
+let farenheightValue = (celsiusValue * 9)/5 + 32
+
+print("the Celsius \(celsiusValue)°C when converted to farenheight is \(farenheightValue)°F")
