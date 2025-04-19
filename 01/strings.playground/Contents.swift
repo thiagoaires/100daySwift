bd5a9fb071aebbd7b86df4a9a46f3e67f426d34e
Thiago Aires da Silva <makarrao@gmail.com>
2025-04-18 22:11:31 -0300

    commit inicial

---
+++ 01/strings.playground/Contents.swift
@@ -0,0 +1,31 @@
+import Cocoa
+
+var greeting : String = "Hello, pray on ground"
+var name = "thiago"
+name = "Rique"
+
+let character: String = "Daphne"
+
+var playerName = "Roy"
+
+print(playerName)
+
+playerName = "Sam"
+
+print(playerName)
+
+let managerName = "Michael Scott"
+let dogBreed = "Samoyed"
+let meaningOfLife = "how many roads must a man walk down?"
+
+let quote = """
+    Then he tapped a sing
+    saying \"Believe\" and walked away
+    """
+let fileName = "paris.jpg"
+
+print(managerName.count)
+print(dogBreed.uppercased())
+
+print(quote.hasPrefix("Then he"))
+print(fileName.hasSuffix(".jpg"))
