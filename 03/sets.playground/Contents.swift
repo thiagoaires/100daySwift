9813641d2c1620aa0df297ec562dd7b14cb288e7
Thiago Aires da Silva <makarrao@gmail.com>
2025-04-20 18:14:37 -0300

    sets

---
+++ 03/sets.playground/Contents.swift
@@ -0,0 +1,17 @@
+import Cocoa
+
+let actors = Set(
+    [
+        "Denzel Washignton",
+        "Tom Cruise",
+        "Brad Pitt",
+        "John Cena"
+    ])
+
+print(actors)
+var actors2 = Set<String>()
+actors2.insert("Hugh Jackman")
+actors2.insert("Nicolas Cage")
+actors2.insert("Samuel l Jackson")
+actors2.insert("John Travolta")
+print(actors2)
