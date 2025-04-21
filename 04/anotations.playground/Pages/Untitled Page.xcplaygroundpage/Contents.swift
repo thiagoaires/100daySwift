a7ab12b2ae523a420e6e13a740e94fcc7986b22e
Thiago Aires da Silva <makarrao@gmail.com>
2025-04-21 15:32:51 -0300

    annotations

---
+++ 04/anotations.playground/Pages/Untitled Page.xcplaygroundpage/Contents.swift
@@ -0,0 +1,29 @@
+import Cocoa
+
+let surname: String = "Aires"
+var score: Double = 12
+
+
+let playerName = "Roy"
+let pi: Double = 3.141
+
+print(playerName)
+
+var albums : [String] = ["Red","Fearless"]
+var user: [String:String] = ["username": "Thiago"]
+
+var soda : [String] = ["Coke", "Pepsi","soda"]
+var teams : [String] = [String]()
+
+enum UIStyle{
+    case light, dark, system
+}
+
+
+var style : UIStyle = UIStyle.light
+style = .dark
+
+let username: String
+username = "twostraws"
+
+print(username)
