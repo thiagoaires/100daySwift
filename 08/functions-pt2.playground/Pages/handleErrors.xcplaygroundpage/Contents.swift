ba31a0043b64f69af53e1540686e8b86e6f8f9c7
Thiago Aires da Silva <makarrao@gmail.com>
2025-04-24 23:32:09 -0300

    handle errors wip

--- 08/functions-pt2.playground/Pages/handleErrors.xcplaygroundpage/Contents.swift
+++ 08/functions-pt2.playground/Pages/handleErrors.xcplaygroundpage/Contents.swift
@@ -1,5 +1,21 @@
 import Cocoa
 
-enum Error{
-    case Errors, Success
+enum PasswordError: Error{
+    case short, obvious
 }
+ 
+func checkPassword(_ password: String) throws -> String{
+    if password.count < 5 {throw PasswordError.short}
+    if password == "12345" {throw PasswordError.obvious}
+    if(password.count < 10){
+        return "great"
+    }
+    if(password.count < 8){
+        return "ok"
+    }
+
+    return "ok"
+}
+
+//checkPassword("12345")
+let valor = "12345"
